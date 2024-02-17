466a15fb3a457491e9cf6d4cfe4cf7b0be8daa73
Ashish Kumar Singh <djdaydreamer.singh4@gmail.com>
2024-02-17 12:10:19 +0530

    Test case passed for handling empty string cases

--- calculator.js
+++ calculator.js
@@ -5,4 +5,14 @@ function add(numbers) {
     .reduce((a, b) => a + b);
 }
 
+function string_addition(string) {
+  string = string.trim();
+  if (string.trim() === "") {
+    return 0;
+  } else {
+    return Number(string);
+  }
+}
+
 exports.add = add;
+exports.string_addition = string_addition;
