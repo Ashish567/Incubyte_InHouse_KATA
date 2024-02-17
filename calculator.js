47a4eeade35b35f1908c955a96e464fce8b970db
Ashish Kumar Singh <djdaydreamer.singh4@gmail.com>
2024-02-17 12:47:53 +0530

    main logic for processing string as per the task done

--- calculator.js
+++ calculator.js
@@ -7,16 +7,18 @@ function add(numbers) {
 
 function string_addition(string) {
   string = string.trim();
-  string = string
-    .split("")
-    .filter((el) => el !== "")
-    .join("");
   if (string.trim() === "") {
     return 0;
   }
+  string = string
+    .split("")
+    .filter((el) => el !== "")
+    .filter((char) => !isNaN(char) === true && NaN !== char)
+    .map((ch) => parseInt(ch));
+
   let nums = [];
-  string.split(",").forEach((element) => {
-    if (typeof element === "number") {
+  string.forEach((element) => {
+    if (isNaN(element) === false) {
       nums.push(Number(element));
     }
   });
--- calculator.test.js
+++ calculator.test.js
@@ -9,7 +9,18 @@ test("string with a single number should result in the number itself", () => {
 test("string with a single number should result in the number itself", () => {
   expect(calculator.add("1,2,3")).toBe(6);
 });
+test("string with two numbers as comma seperated string should result in their sum", () => {
+  expect(calculator.string_addition("1\n2,3")).toBe(6);
+});
+
+test("string with two numbers as comma seperated string and various delimeters should result in their sum", () => {
+  expect(calculator.string_addition("//[***]\n1***2***3")).toBe(6);
+});
+
 // test("string with two numbers as comma seperated string should result in their sum", () => {
-//   //   expect(calculator.string_addition("1\n2,3")).toBe(3);
-//   expect(calculator.string_addition("1\n2,3")).toBe([1, 2, 3]);
+//   expect(
+//     calculator.string_addition(
+//       "//[delim1][delim2]\n” for example “//[*][%]\n1*2%3"
+//     )
+//   ).toBe(6);
 // });
